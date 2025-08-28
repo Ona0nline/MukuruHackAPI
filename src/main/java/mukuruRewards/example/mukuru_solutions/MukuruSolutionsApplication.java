18dbd393da8f60054b4aff10e54b0a772ffa6f0f
Ona0nline <onasihle123@gmail.com>
2025-08-28 13:03:31 +0200

    Initial push

---
+++ src/main/java/mukuruRewards/example/mukuru_solutions/MukuruSolutionsApplication.java
@@ -0,0 +1,13 @@
+package mukuruRewards.example.mukuru_solutions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MukuruSolutionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MukuruSolutionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mukuruRewards/example/mukuru_solutions/MukuruSolutionsApplicationTests.java
@@ -0,0 +1,13 @@
+package mukuruRewards.example.mukuru_solutions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MukuruSolutionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
